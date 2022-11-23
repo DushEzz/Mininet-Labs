206fda20861fc69b0dcc102fd4e1711f6cdd1fb7
Andrew <dushez.baranov@gmail.com>
2022-11-23 23:19:59 +0300

    Fixed encoding error

--- CustomTopo.py
+++ CustomTopo.py
@@ -25,44 +25,37 @@ class CustomTopo(Topo):
 
         core = self.addSwitch('core_ty1')
 
-        # Вершина топологии
         for a in range(fanout):
             aggr = self.addSwitch('a%s' % (a + 1))
-
-            # Добавляем свитч в топологию
             self.addLink(aggr, core, **linkopts1)
 
-            # Связываем свитч этого уровня с кор-свичом, распаковываем настройки производительности
             for e in range(fanout):
                 edge = self.addSwitch('e%s' % ((a * fanout + e) + 1))
-
-                # Каждому а-свичу добавлем edge свитч
                 self.addLink(edge, aggr, **linkopts2)
 
                 for h in range(fanout):
                     host = self.addHost('h%s' % (((a * fanout + e) * fanout + h) + 1))
-                    # Добавляем хосты
                     self.addLink(host, edge, **linkopts3)
 
 
 def simple_test():
-    print("1. --Задаем параметры")
+    print("1. --Set parameters")
     linkopts1 = {'bw': 50, 'delay': '5ms'}
     linkopts2 = {'bw': 30, 'delay': '10ms'}
     linkopts3 = {'bw': 10, 'delay': '15ms'}
 
-    print("2. --Реализуем нашу топологию с праметрами")
+    print("2. --Realising our topology with parameters")
     topo = CustomTopo(linkopts1, linkopts2, linkopts3, fanout=2)
 
-    print("3. --Создаем сеть из нашей топологии ")
+    print("3. --Creating a network from our topology")
     net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
     net.start()
     dumpNodeConnections(net.hosts)
 
-    print("4. --Тестируем сеть ")
+    print("4. --Testing the network")
     net.pingAll()
 
-    print("5. --Останавливаем")
+    print("5. --Stopping")
     net.stop()
 
 
