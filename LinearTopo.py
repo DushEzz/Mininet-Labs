34f0cf1789e73e0081f8056436357f7c8682f386
Andrew <dushez.baranov@gmail.com>
2022-11-23 22:02:35 +0300

    Fixed indent blocks

--- LinearTopo.py
+++ LinearTopo.py
@@ -7,34 +7,35 @@ from mininet.link import TCLink
 from mininet.util import irange, dumpNodeConnections
 from mininet.log import setLogLevel
 
+
 class LinearTopo(Topo):
-    "Linear topology of k switches, with one host per switch."
+    """Linear topology of k switches, with one host per switch."""
 
     def __init__(self, k=2, **opts):
-    """
-    Init.
-    k: number of switches (and hosts)
-    hconf: host configuration options
-    lconf: link configuration options
-    """
-
-    super(LinearTopo, self).__init__(**opts)
-
-    self.k = k
-
-    lastSwitch = None
-    for i in irange(1, k):
-        host = self.addHost('h%s' % i, cpu=.5/k)
-        switch = self.addSwitch('s%s' % i)
-        # 10 Mbps, 5ms delay, 1% loss, 1000 packet queue
-        # self.addLink(host, switch, bw=10, delay='5ms', loss=1, max_queue_size=1000, use_htb=True)
-        if lastSwitch:
-            self.addLink(switch, lastSwitch, bw=10, delay='5ms', loss=1,max_queue_size=1000, use_htb=True)
-        lastSwitch = switch
+        """
+        Init.
+        k: number of switches (and hosts)
+        hconf: host configuration options
+        lconf: link configuration options
+        """
+    
+        super(LinearTopo, self).__init__(**opts)
+    
+        self.k = k
+    
+        lastSwitch = None
+        for i in irange(1, k):
+            host = self.addHost('h%s' % i, cpu=.5/k)
+            switch = self.addSwitch('s%s' % i)
+            # 10 Mbps, 5ms delay, 1% loss, 1000 packet queue
+            # self.addLink(host, switch, bw=10, delay='5ms', loss=1, max_queue_size=1000, use_htb=True)
+            if lastSwitch:
+                self.addLink(switch, lastSwitch, bw=10, delay='5ms', loss=1,max_queue_size=1000, use_htb=True)
+            lastSwitch = switch
 
 
 def perfTest():
-    "Create network and run simple performance test"
+    """Create network and run simple performance test"""
 
     topo = LinearTopo(k=4)
     net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
